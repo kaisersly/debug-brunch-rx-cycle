c530482eda4beed67d1c46c0383f7c4af93345c0
Sylvain Kieffer <sylvain.kieffer@univ-paris13.fr>
2016-02-29 14:17:45 +0100

    second commit

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log(require('rx'));
   console.log(require('@cycle/core'));
+  console.log(require('@cycle/dom'));
   console.log('Initialized app');
 });
